c202fb87874677c60af4053150d4c6f03840a383
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2026-02-25 16:58:38 -0500

    Reorder benchmarks?

--- examples/json.ts
+++ examples/json.ts
@@ -6,12 +6,12 @@ const RUN_NATIVE = true;
 const LOG_GRAPH = false;
 const LOG_NUMBERS = true;
 const RUN_N = 10;
-const RUN_BENCHMARK = false;
+const RUN_BENCHMARK = true;
 const LOG_ASTIR = false;
 const LOG_DATA = false;
 
-const input = await (await fetch('https://microsoftedge.github.io/Demos/json-dummy-data/5MB.json')).text();
-// const input = await readFile('./json_sample1k.json', 'utf-8');
+// const input = await (await fetch('https://microsoftedge.github.io/Demos/json-dummy-data/5MB.json')).text();
+const input = await readFile('./json_sample1k.json', 'utf-8');
 console.log('Input: ', input.length);
 
 // -----------------------------------------------------------------------
@@ -143,16 +143,16 @@ import Benchmark from 'benchmark';
 
 const suite = new Benchmark.Suite('json-test');
 
-suite.add("parser", () => {
-  const result = parseJSON(input, 'Entry');
-});
-
 suite.add("combined", () => {
   const result = parseJSON(input, 'Entry');
   const astIR = transformCSTRoot(result);
   const typedAST = toTypedAST(astIR);
   const data = jsonSemantics.evaluate(typedAST);
   data;
+});
+
+suite.add("parser", () => {
+  const result = parseJSON(input, 'Entry');
 });
 
 suite.on('cycle', function (event: Benchmark.Event) {
